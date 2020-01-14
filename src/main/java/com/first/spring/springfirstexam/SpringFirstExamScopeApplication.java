731db65ad6c0b4773eafdf4a040b642854274dd9
Mehrdad <rashidi.mehrdad@gmail.com>
2020-01-14 22:50:52 +0330

    3th commit

---
+++ src/main/java/com/first/spring/springfirstexam/SpringFirstExamScopeApplication.java
@@ -0,0 +1,26 @@
+package com.first.spring.springfirstexam;
+
+import com.first.spring.springfirstexam.basic.BinarySearchImpl;
+import com.first.spring.springfirstexam.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class SpringFirstExamScopeApplication {
+	private static Logger logger = (Logger) LoggerFactory.getLogger(SpringFirstExamScopeApplication.class);
+	public static void main(String[] args) {
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringFirstExamScopeApplication.class, args);
+		PersonDAO personDAO  = applicationContext.getBean(PersonDAO.class);
+		PersonDAO personDAO2  = applicationContext.getBean(PersonDAO.class);
+		logger.info("{}",personDAO);
+		logger.info("{}",personDAO.getJdbcConnection());
+		logger.info("{}",personDAO2);
+		logger.info("{}",personDAO2.getJdbcConnection());
+	}
+
+}
